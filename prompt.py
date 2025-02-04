0c779b48635bd565713e95ad25083a79413eab07
AnyigorTobias <anuelsontobby@g,ail.com>
2025-02-04 14:26:57 +0100

    Added a prompt script and cleaned up code

---
+++ prompt.py
@@ -0,0 +1,18 @@
+role = "You are an expert career consultant"
+
+instruction = [
+                  "Use your superior SEO skills to search the web for solutions",
+                  "Let your recommendation contain links to the each opportunity",
+                  "Ensure that the recommendations have not exceeded deadline dates",
+                  "search www.opportunitiesforafricans.com",
+                  "search www.opportunitydesk.com",
+                  
+    ]
+
+task = "Recommend the best 10 opportunitties to the user"
+
+
+output = "Present your result in a tabular form. Let the recommendation move from the most suitable opportunity to the less suitable one, add the deadline dates, remember to display the dates and links to application"
+
+
+user_prompt = "Recommend latest opportunities (they must be from 2025 upwards) for a 27year old male bachelor with a degree in Chemical engineering, data science certificatiosn and interest in techbusiness and Ai startup"
--- search.py
+++ search.py
@@ -2,6 +2,7 @@
 from phi.agent import Agent
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.model.groq import Groq
+from prompt import user_prompt, instruction, task, output, role
 
 #from pydantic import BaseModel, Field
 #from typing import List, Dict, Any, Tuple, Optional
@@ -19,33 +20,25 @@ load_dotenv()
 
 agent = Agent(
     #work on the prompt template
-    description="You are an expert career consultant",
-    instructions=[
-                  "Use your superior SEO skills to search the web for solutions",
-                  "Let your recommendation contain links to the each opportunity",
-                  "Ensure that the recommendations have not exceeded deadline dates",
-                  "search www.opportunitiesforafricans.com",
-                  "search www.opportunitydesk.com",
-                  
-    ],
+    description=role,
+    instructions=instruction,
                  
-    task = "Recommend the best 10 opportunitties to the user",
-    expected_output = "Present your result in a tabular form. Let the recommendation move from the most suitable opportunity to the less suitable one, add the deadline dates, remember to display the dates",
+    task = task,
+    expected_output = output,
     add_datetime_to_instructions = True,
     markdown=True,
     debug_mode=True,
     
     
     
-    #optimize the duckduckgo search
+    #the duckduckgo search
     tools=[DuckDuckGo(search=True)], show_tool_calls=True,
     
     
-    #optimize the llm
-    model=Groq(id="deepseek-r1-distill-llama-70b"),
-    temperature = 0.3 )
+    #set and customize the LLM the llm
+    model=Groq(id="llama-3.3-70b-versatile"),
+    temperature = 0.2,
+    )
 
 
-
-
-agent.print_response("Recommend latest opportunities for a 27year old male bachelor with a degree in Chemical engineering, data science certificatiosn and interest in techbusiness and Ai startup")
+agent.print_response(user_prompt)
