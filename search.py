6302667804d4ab3d79e12b3545ca0f3c2d224bee
AnyigorTobias <anuelsontobby@g,ail.com>
2025-02-05 13:51:38 +0100

    expanded the possibilities of search.py by adding google search and adding agent memory

--- search.py
+++ search.py
@@ -1,8 +1,17 @@
 
 from phi.agent import Agent
 from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.googlesearch import GoogleSearch
 from phi.model.groq import Groq
 from prompt import user_prompt, instruction, task, output, role
+
+from phi.storage.agent.sqlite import SqlAgentStorage
+storage = SqlAgentStorage(
+    # store sessions in the ai.sessions table
+    table_name="agent_sessions",
+    # db_file: Sqlite database file
+    db_file="tmp/data.db",
+)
 
 #from pydantic import BaseModel, Field
 #from typing import List, Dict, Any, Tuple, Optional
@@ -18,7 +27,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-agent = Agent(
+duck_agent = Agent(
     #work on the prompt template
     description=role,
     instructions=instruction,
@@ -38,7 +47,24 @@ agent = Agent(
     #set and customize the LLM the llm
     model=Groq(id="llama-3.3-70b-versatile"),
     temperature = 0.2,
+    
+    #storage
+    storage = SqlAgentStorage(table_name="duck", db_file="agents.db")
     )
 
 
-agent.print_response(user_prompt)
+
+
+google_agent = Agent(
+    tools=[GoogleSearch()],
+    description=role,
+    instructions=instruction,
+    task = task,
+    expected_output= output,
+    show_tool_calls=True,
+    debug_mode=True,
+    model=Groq(id="llama-3.3-70b-versatile"),
+    temperature = 0.2,
+    storage = SqlAgentStorage(table_name="google", db_file="agents.db")
+)
+google_agent.print_response(user_prompt, markdown=True)
